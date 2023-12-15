63cb87e36f3bbd601a189be8b8f6927366e0d277
Jordi Sabrià Pagès <jordisabria@hotmail.com>
2023-12-15 19:16:25 +0100

    Initial Commit

---
+++ Concurrencia_Ejercicios/Concurrencia_EjerciciosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrencia_EjerciciosApp.swift
+//  Concurrencia_Ejercicios
+//
+//  Created by Jordi Sabrià Pagès on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Concurrencia_EjerciciosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrencia_Ejercicios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concurrencia_Ejercicios
+//
+//  Created by Jordi Sabrià Pagès on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
